04c35725373029c1ef46016d0b1d020228946833
Kostas Andreadis <kandread@umass.edu>
2022-12-26 15:41:32 -0500

    Updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using Sad
 using Test
 
-@testset "Sad.jl" begin
-    # Write your tests here.
+@testset "Cross-sections" begin
+    r_xs = Rectangular(100., 5., 2., 0.00001, 0.03)
+    d_xs = Dingman(100., 5., 2., 2.5, 0.00001, 0.03)
+    @test width(r_xs) == 100.0
+    @test depth(r_xs) == 2.0
+    @test width(d_xs) ≈ 69.314 atol=0.001
+    @test depth(d_xs) ≈ 1.428 atol=0.001
 end
