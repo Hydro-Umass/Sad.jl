fb58d67dc168ac4987a54eea8220e1077a84375b
Kostas Andreadis <kandread@umass.edu>
2023-01-06 09:19:36 -0500

    Fixes #11. Restore localization in LETKF

--- src/kalman.jl
+++ src/kalman.jl
@@ -2,7 +2,7 @@ using LinearAlgebra
 using Statistics
 
 """
-    letkf(A, d, HA, E, ρ, diagR)
+    letkf(A, d, HA, E, xs, ys, ρ, diagR)
 
 Apply the Local Ensemble Transform Kalman Filter algorithm.
 
@@ -12,13 +12,15 @@ Apply the Local Ensemble Transform Kalman Filter algorithm.
 - `d`: observation vector
 - `HA`: model-predicted observation ensemble matrix
 - `E`: observation error ensemble matrix
+- `xs`: indices of state vector that map to observation local patches
+- `ys`: indices of observation local patches
 - `ρ`: covariance inflation parameter (optional, default value of 1.05)
 - `diagR`: force observation error covariance to be diagonal, i.e. no spatial correlation (optional, default is false)
 
 """
-function letkf(A::Matrix{Float64}, d::Vector{Float64}, HA::Matrix{Float64}, E::Matrix{Float64}; ρ::Float64=1.05, diagR::Bool=false)
+function letkf(A::Matrix{Float64}, d::Vector{Float64}, HA::Matrix{Float64}, E::Matrix{Float64}, xs::Union{Nothing, Vector{Vector{Int}}}=nothing, ys::Union{Nothing, Vector{Vector{Int}}}=nothing; ρ::Float64=1.05, diagR::Bool=false)
     Aa = zeros(size(A))
-    _, nens = size(A)
+    ndim, nens = size(A)
     nobs = length(d)
     if size(E) == (nobs, nobs)
         R = E
@@ -31,13 +33,21 @@ function letkf(A::Matrix{Float64}, d::Vector{Float64}, HA::Matrix{Float64}, E::M
     end
     Y = HA .- mean(HA, dims=2)
     X = A .- mean(A, dims=2)
-    # FIXME restore localization for the analysis step
-    C = Y' * pinv(R)
-    P = pinv((nens - 1) / ρ * I + C * Y)
-    W = real(sqrt((nens - 1) * P))
-    w = P * C * (d .- mean(HA, dims=2))
-    W = W .+ w
-    Xᵃ = X * W .+ mean(A, dims=2)
-    Aa = Xᵃ
+    if isnothing(xs) | isnothing(ys)
+        xs = [collect(1:ndim)]
+        ys = [collect(1:nobs)]
+    end
+    for (lx, ly) in zip(xs, ys)
+        Xₗ = X[lx, :]
+        Yₗ = Y[ly, :]
+        Rₗ = R[ly, ly]
+        C = Yₗ' * pinv(Rₗ)
+        P = pinv((nens - 1) / ρ * I + C * Yₗ)
+        W = real(sqrt((nens - 1) * P))
+        w = P * C * (d[ly] .- mean(HA[ly, :], dims=2))
+        W = W .+ w
+        Xₗᵃ = Xₗ * W .+ mean(A[lx, :], dims=2)
+        Aa[lx, :] = Xₗᵃ
+    end
     Aa
 end
